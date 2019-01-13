c945740ccd3e8e8448dd3ec6fad847b43ba3fe24
Ursula Tooley <utooley@gmail.com>
2019-01-13 15:34:11 -0500

    added network segregation analyses

--- 03_tooley_rsfmri_nets_ses_main_analyses_in_r.R
+++ 03_tooley_rsfmri_nets_ses_main_analyses_in_r.R
@@ -25,13 +25,13 @@ library(ppcor)
 ###############
 #get files
 #Local personal computer
-setwd("~/Documents/bassett_lab/tooleyEnviNetworks/data/rest/")
-subinfodir="~/Documents/bassett_lab/tooleyEnviNetworks/data/subjectData/"
-sublistdir="~/Documents/bassett_lab/tooleyEnviNetworks/subjectLists/"
-qadir="~/Documents/bassett_lab/tooleyEnviNetworks/data/rest/"
-clustcodir="~/Dropbox (Personal)/bassett_lab/clustco_paper/"
-analysis_dir="~/Documents/bassett_lab/tooleyEnviNetworks/analyses/"
-reho_dir="~/Documents/bassett_lab/tooleyEnviNetworks/data/rest/"
+setwd("~/Documents/projects/in_progress/tooleyEnviNetworks/data/rest/")
+subinfodir="~/Documents/projects/in_progress/tooleyEnviNetworks/data/subjectData/"
+sublistdir="~/Documents/projects/in_progress/tooleyEnviNetworks/subjectLists/"
+qadir="~/Documents/projects/in_progress/tooleyEnviNetworks/data/rest/"
+clustcodir="~/Dropbox (Personal)/projects/in_progress/tooleyenvinetworks/code/clustco_paper/"
+analysis_dir="~/Documents/projects/in_progress/tooleyEnviNetworks/output/"
+reho_dir="~/Documents//projects/in_progress/tooleyEnviNetworks/data/rest/"
 
 #Local mackey computer
 setwd("~/Documents/bassett_lab/tooleyEnviNetworks/data/rest/")
@@ -53,9 +53,9 @@ envdata <- read.csv(paste0(subinfodir, "Census_for_Scores.csv"))
 #Get QA values to include in analyses
 file3<-read.csv(paste0(qadir, "n1601_RestQAData_20170509.csv"))
 #Get the network stats file with the different clustering coefficients
-file4<-read.csv("~/Documents/bassett_lab/tooleyEnviNetworks/analyses/n1012_net_meas_normed3clustcoefs_signed.csv")
+file4<-read.csv(paste0(analysis_dir,"n1012_net_meas_normed3clustcoefs_signed.csv"))
 #get the original file with modularity in it
-file4<-read.csv("~/Documents/bassett_lab/tooleyEnviNetworks/analyses/n1012_sub_net_meas_signed.csv")
+file4<-read.csv(paste0(analysis_dir,"n1012_sub_net_meas_signed.csv"))
 #rename the second column of the network statistics file to be 'scanid' so it matches the other files
 file4<-dplyr::rename(file4, scanid=subjlist_2)
 
@@ -218,6 +218,113 @@ l <- lm(avgclustco_both.x ~ ageAtScan1yrs+sex+race2+avgweight.x+modul+envSEShigh
 summary(l)
 l <- lm(avgclustco_both.x ~ ageAtScan1yrs+sex+race2+avgweight.x+modul+envSEShigh+restRelMeanRMSMotion+ageAtScan1yrs*envSEShigh, data=master)
 summary(l)
+
+##### NETWORK SEGREGATION PER CHAN ET AL. 2018 ####
+
+#read in file, add to rest of data
+file7 <- read.csv("~/Dropbox (Personal)/projects/in_progress/tooleyenvinetworks/data/n1012_sub_net_meas_signed_w_segregation.csv")
+file7<-dplyr::rename(file7, scanid=subjlist_2)
+master <- master %>% select(.,- (avgweight:modul)) #remove first set of these variables so names don't overlap, already checked that they are exactly the same
+master<-right_join(file7, master, by ="scanid")
+
+#is system segregation correlated with modularity or avg clustco?
+cor.test(master$modul,master$sys_segreg,method = "spearman")
+cor.test(master$avgclustco_both,master$sys_segreg,method = "spearman")
+
+#when controlling for other variables?
+temp<-cbind(master$ageAtScan1cent, as.factor(master$sex), as.factor(master$race2), master$avgweight, master$restRelMeanRMSMotion, as.factor(master$envSEShigh))
+library(ppcor)
+pcor.test(master$modul, master$sys_segreg, temp, method = "spearman")
+pcor.test(master$avgclustco_both, master$sys_segreg, temp, method = "spearman")
+
+#non-linear relationship with age?
+ageonlyRlrtmodel<-gamm(sys_segreg~s(ageAtScan1cent)+sex+race2+avgweight+restRelMeanRMSMotion+envSEShigh, method='REML', data=master)$lme
+l<-exactRLRT(ageonlyRlrtmodel) #yes,there is.
+#What does it look like?
+l <- gam(sys_segreg~s(ageAtScan1cent)+sex+race2+avgweight+restRelMeanRMSMotion+envSEShigh, method='REML', data=master)
+summary(l)
+plot.gam(l)
+plotGAM(l, "ageAtScan1cent")
+visreg(l, "ageAtScan1cent")
+ggplot.model(l) #need script gam_plot_code to run
+
+#linear age effect without interaction
+l <- lm(sys_segreg ~ ageAtScan1yrs+sex+race2+avgweight+envSEShigh+restRelMeanRMSMotion, data=master)
+summary(l)
+lm.beta(l)
+
+#linear interaction model med split
+l2 <- lm(sys_segreg ~ ageAtScan1yrs+sex+race2+avgweight+restRelMeanRMSMotion+envSEShigh+ageAtScan1yrs*envSEShigh, data=master)
+summary(l2)
+lm.beta(l2)
+#linear interaction model cont SES
+l2 <- lm(sys_segreg ~ ageAtScan1yrs+sex+race2+avgweight+restRelMeanRMSMotion+envSES+ageAtScan1yrs*envSES, data=master)
+summary(l2)
+lm.beta(l2)
+
+#Non-linear model of age
+l <- gam(sys_segreg~s(ageAtScan1cent)+sex+race2+avgweight+restRelMeanRMSMotion+envSES, method='REML', data=master)
+summary(l)
+lm.beta(l)
+
+#Non-linear model of age, med split interaction
+l <- gam(sys_segreg~s(ageAtScan1cent)+s(ageAtScan1cent, by=envSEShigh)+sex+race2+avgweight+restRelMeanRMSMotion+envSEShigh, method='REML', data=master)
+summary(l)
+
+#Non-linear model of age, continuous interaction
+l <- gam(sys_segreg~s(ageAtScan1cent)+s(ageAtScan1cent, by=envSES)+sex+race2+avgweight+restRelMeanRMSMotion+envSES, method='REML', data=master)
+summary(l)
+#Looked at it with pbmodcomp elsewhere.
+
+##### NETWORK SEGREGATION PER CHAN ET AL. 2018, POSITIVE ONLY ####
+#is pos system segregation correlated with modularity or avg clustco?
+cor.test(master$modul,master$sys_segreg_posonly,method = "spearman")
+cor.test(master$avgclustco_both,master$sys_segreg_posonly,method = "spearman")
+
+#when controlling for other variables?
+temp<-cbind(master$ageAtScan1cent, as.factor(master$sex), as.factor(master$race2), master$avgweight, master$restRelMeanRMSMotion, as.factor(master$envSEShigh))
+library(ppcor)
+pcor.test(master$modul, master$sys_segreg_posonly, temp, method = "spearman")
+pcor.test(master$avgclustco_both, master$sys_segreg_posonly, temp, method = "spearman")
+
+#non-linear relationship with age?
+ageonlyRlrtmodel<-gamm(sys_segreg_posonly~s(ageAtScan1cent)+sex+race2+avgweight+restRelMeanRMSMotion+envSES, method='REML', data=master)$lme
+l<-exactRLRT(ageonlyRlrtmodel) #yes,there is.
+#What does it look like?
+l <- gam(sys_segreg_posonly~s(ageAtScan1cent)+sex+race2+avgweight+restRelMeanRMSMotion+envSEShigh, method='REML', data=master)
+summary(l)
+plot.gam(l)
+plotGAM(l, "ageAtScan1cent")
+visreg(l, "ageAtScan1cent")
+ggplot.model(l) #need script gam_plot_code to run
+
+#linear age effect without interaction
+l <- lm(sys_segreg ~ ageAtScan1yrs+sex+race2+avgweight+envSEShigh+restRelMeanRMSMotion, data=master)
+summary(l)
+lm.beta(l)
+
+#linear interaction model med split
+l2 <- lm(sys_segreg ~ ageAtScan1yrs+sex+race2+avgweight+restRelMeanRMSMotion+envSEShigh+ageAtScan1yrs*envSEShigh, data=master)
+summary(l2)
+lm.beta(l2)
+#linear interaction model cont SES
+l2 <- lm(sys_segreg ~ ageAtScan1yrs+sex+race2+avgweight+restRelMeanRMSMotion+envSES+ageAtScan1yrs*envSES, data=master)
+summary(l2)
+lm.beta(l2)
+
+#Non-linear model of age
+l <- gam(sys_segreg~s(ageAtScan1cent)+sex+race2+avgweight+restRelMeanRMSMotion+envSES, method='REML', data=master)
+summary(l)
+lm.beta(l)
+
+#Non-linear model of age, med split interaction
+l <- gam(sys_segreg~s(ageAtScan1cent)+s(ageAtScan1cent, by=envSEShigh)+sex+race2+avgweight+restRelMeanRMSMotion+envSEShigh, method='REML', data=master)
+summary(l)
+
+#Non-linear model of age, continuous interaction
+l <- gam(sys_segreg~s(ageAtScan1cent)+s(ageAtScan1cent, by=envSES)+sex+race2+avgweight+restRelMeanRMSMotion+envSES, method='REML', data=master)
+summary(l)
+#Looked at it with pbmodcomp elsewhere.
 
 ##############
 #### FIGURE 3 : YEO SYSTEMS ###
